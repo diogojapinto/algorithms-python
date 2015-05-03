4c459e26b17a6598bf4c3fd97a36f21572f5fa1b
diogojapinto <diogojapinto@gmail.com>
2015-05-03 21:40:45 +0100

    tsp delivered, now trying 2-sat by local search

--- 2sat.py
+++ 2sat.py
@@ -3,13 +3,15 @@
 """
 
 import sys
+import math
+import random
 
-def main():
+def main(filename):
     """ Main function here """
 
     # Parse file
-    in_file = open(sys.argv[1])
-    nr_variables = int(in_file.strip())
+    in_file = open(filename)
+    nr_variables = int(in_file.readline().strip())
 
     clauses = []
 
@@ -17,9 +19,60 @@ def main():
         var1, var2 = [int(x) for x in line.strip().split()]
         clauses.append((var1, var2))
 
-    
+    for _ in range(math.floor(math.log(nr_variables, 2))):
+        #variables = [random.choice([False, True]) for _ in range(nr_variables)]
+        variables = init_variables(clauses, nr_variables)
+        for _ in range(2 * nr_variables ** 2):
+            valid, clause_nr = verify_satisfiability(variables, clauses)
+            if valid:
+                print("True")
+                return
+            else:
+                pivot = random.randint(0, 1)
+                var_i = abs(clauses[clause_nr][pivot]) - 1
+                variables[var_i] = not variables[var_i]
 
+    print(False)
+
+def init_variables(clauses, nr_variables):
+    """ Initializes the variables approximately to the clauses """
+
+    random.shuffle(clauses)
+    variables = [random.choice([False, True]) for _ in range(nr_variables)]
+
+    for clause in clauses:
+        var1 = abs(clause[0]) - 1
+        var2 = abs(clause[1]) - 1
+        truth1 = clause[0] >= 0
+        truth2 = clause[1] >= 0
+
+        variables[var1] = truth1
+        variables[var2] = truth2
+
+    return variables
+
+
+
+def verify_satisfiability(variables, clauses):
+    """ Do I really need to explain this? """
+    
+    for i, clause in enumerate(clauses):
+        var1 = abs(clause[0]) - 1
+        var2 = abs(clause[1]) - 1
+        truth1 = clause[0] >= 0
+        truth2 = clause[1] >= 0
+
+        if not (variables[var1] == truth1 or variables[var2] == truth2):
+            return False, i
+
+    return True, None
 
 
 if __name__ == '__main__':
-    main()
+    #main("2sat1.txt")
+    #main("2sat2.txt")
+    #main("2sat3.txt")
+    #main("2sat4.txt")
+    #main("2sat5.txt")
+    #main("2sat6.txt")
+    main("2sat.txt")
--- tsp.py
+++ tsp.py
@@ -4,14 +4,16 @@
 
 from itertools import combinations
 from math import sqrt
+import matplotlib.pyplot as plt
 
 INFINITY = float('inf')
 
+#@profile
 def main():
     """ Main function here """
 
     # Parse the file
-    in_file = open('tsp.txt')
+    in_file = open('tsp2.txt')
     _ = int(in_file.readline().strip()) # nr_cities
 
     cities = []
@@ -20,45 +22,59 @@ def main():
         x_coord, y_coord = [float(x) for x in line.strip().split()]
         cities.append((x_coord, y_coord))
 
-    starting_point = cities[0]
+    starting_point = (0, cities[0])
+
+    distances = precompute_distances(cities)
+
+    #plt.plot([x[0] for x in cities], [x[1] for x in cities], 'ro')
+    #plt.show()
+    #return
 
     # main loop
     solutions = {}
-    solutions[frozenset(starting_point)] = [INFINITY] * len(cities)
-    solutions[frozenset(starting_point)][0] = 0
+    solutions[frozenset([starting_point])] = [INFINITY] * len(cities)
+    solutions[frozenset([starting_point])][0] = 0
+    for nr_elems in range(1, len(cities)):
+        new_solutions = {}
 
-    for vertex_set in get_ordered_subsets(cities[1:]):
-        vertex_set = vertex_set.union({(0, starting_point)})
-        solutions[vertex_set] = [INFINITY] * len(cities)
+        for vertex_set in get_ordered_subsets(cities[1:], nr_elems):
+            vertex_set = vertex_set.union({starting_point})
+            new_solutions[vertex_set] = [INFINITY] * len(cities)
 
-        for j, vertex1 in vertex_set:
-            if vertex1 == starting_point:
-                continue
-
-            min_solution = INFINITY
-            old_set = vertex_set.difference({(j, vertex1)})
-            for k, vertex2 in vertex_set:
-                if vertex1 == vertex2 or vertex2 == starting_point:
+            for j, vertex1 in vertex_set:
+                if j == 0:
                     continue
 
-                new_solution = solutions[old_set][k] + euclidian_distance(vertex1, vertex2)
-                min_solution = min(min_solution, new_solution)
+                min_solution = INFINITY
+                old_set = vertex_set.difference({(j, vertex1)})
 
-            solutions[vertex_set][j] = min_solution
+                for k, vertex2 in vertex_set:
+                    if j == k:
+                        continue
+                    min_solution = min(min_solution,
+                                       solutions[old_set][k] + 
+                                       distances[vertex1][vertex2])
+
+                new_solutions[vertex_set][j] = min_solution
+
+        solutions = new_solutions
 
     min_solution = INFINITY
-    for j, value in enumerate(solutions[frozenset(cities)]):
-        new_solution = value + euclidian_distance(cities[j], starting_point)
-        min_solution = min(min_solution, new_solution)
+    for key in solutions:
+        if len(key) != len(cities):
+            continue
 
-    return min_solution
+        for j, value in enumerate(solutions[key]):
+            new_solution = value + euclidian_distance(cities[j], starting_point[1])
+            min_solution = min(min_solution, new_solution)
+
+    print(min_solution)
 
 
-def get_ordered_subsets(lst):
+def get_ordered_subsets(lst, nr_elems):
     """ Generator function that returns an cardinality-ordered permutation of the list """
-    for nr_elems in range(1, len(lst)):
-        for combination in combinations(enumerate(lst, 1), nr_elems):
-            yield frozenset(combination)
+    for combination in combinations(enumerate(lst, 1), nr_elems):
+        yield frozenset(combination)
 
 def euclidian_distance(point1, point2):
     """ Computes the euclidian distance (I know the name says everything, but 
@@ -66,6 +82,17 @@ def euclidian_distance(point1, point2):
         good cause I do them anyway) """
     return sqrt((point2[0] - point1[0]) ** 2 +
                 (point2[1] - point1[1]) ** 2)
+
+def precompute_distances(points):
+    """ Noneed to explain this either """
+
+    distances = {}
+    for point1 in points:
+        distances[point1] = {}
+        for point2 in points:
+            distances[point1][point2] = euclidian_distance(point1, point2)
+
+    return distances
 
 
 if __name__ == '__main__':
